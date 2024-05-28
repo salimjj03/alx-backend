e61a6b0512d4517448641962f7a37ed82e927e1a
salimaliyu <salemaliyu1992@gmail.com>
2024-05-28 11:48:26 +0000

    i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This is the flask app module
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """
+    the home page raute
+    """
+
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
